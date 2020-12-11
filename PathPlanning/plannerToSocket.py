aecfc088759242ce7fec856fcdae6e4c69213fef
mkscheble <mkscheble@yahoo.com>
2020-12-10 18:13:16 -0800

    waypoint test as transform

--- PathPlanning/plannerToSocket.py
+++ PathPlanning/plannerToSocket.py
@@ -3,14 +3,10 @@ from pathFunc import *
 
 sio = socketio.Client()
 
-states = {'roll':0, 'pitch':0, 'left_castor_angle':0, 'right_castor_angle':0,
-        'linear_velocity': 0, 'angular_velocity': 0}
-
-
 @sio.event
 def connect():
-    sio.emit("subscribe", ["/caster_angle/left", "/castor_angle/right",
-    "/wheel_speed/left", "/wheel_speed/right", "/orientation", "/drive_cmd"])
+    # sio.emit("subscribe", ["/waypoints"])
+    sio.emit("/path", [0,0,0,0,0,0])
     print('Connected successfully')
 
 @sio.event
@@ -20,48 +16,27 @@ def connect_error():
 def disconnect():
     print('Connection Lost')
 
-@sio.on('/orientation')
-def imu(data):
-    states['roll']=  data['roll']
-    states['pitch'] = data['yaw']
-
-@sio.on('/caster_angle/left')
-def caster_left(data):
-    states['left_castor_angle'] = data['yaw']
-
-@sio.on('/caster_angle/right')
-def caster_right(data):
-    states['right_castor_angle'] = data['yaw']
-
-@sio.on('/wheel_speed/left')
-def left_wheel(data):
-    left_wheel_speed = data
-
-@sio.on('/wheel_speed/right')
-def right_wheel(data):
-    right_wheel_speed = data
-
-
 #need to figure out how to recieve waypoints, computer vision aspect and how we comunicate those inputs on socket.io
 
-@sio.on('/drive_cmd') #is this supposed to be an event? Maybe we can call this function when computer vision is done
+@sio.on('/waypoints') #is this supposed to be an event? Maybe we can call this function when computer vision is done
 def pathPlan(data):
     #insert data reorganization here for functions
-    new_data = ...
-    mx_vel = ...
-    mx_accel = ...
-    mx_jerk = ...
-    dt =  0.05
-    widthWheels = ...
+    # new_data = ...
+    # mx_vel = ...
+    # mx_accel = ...
+    # mx_jerk = ...
+    # dt =  0.05
+    # widthWheels = ...
     pts = createPoints(new_data)
-    info, trajectory = path(pts, mx_vel, mx_accel, mx_jerk, dt)
+    info, trajectory = path(pts)
     right, left = rightLeft(trajectory, widthWheels)
     #need to figure out where to put this or emit to, probably should go to controller, right
     #right/left list of these <Segment dt=0.050000 x=-4.174969 y=-1.178583 position=0.002500 velocity=0.100000 acceleration=2.000000 jerk=40.000000 heading=5.498103>
     #this is how right and left look, large list of these
-    sio.emit('/trajectory', trajectory)
+    sio.emit('/path', trajectory)
 
-@sio.event
-def setWay(waypoints):
-    sio.emit('/path', waypoints)
+# @sio.event
+# def setWay(waypoints):
+#     sio.emit('/path', waypoints)
+
 sio.connect('http://localhost:3000')
